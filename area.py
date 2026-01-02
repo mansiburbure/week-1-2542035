86b63bd0f35002004b91c49e213fa10686b284f2
mansiburbure <mansiburbure@gmail.com>
2026-01-02 20:44:56 +0530

    calculation of area using user input

---
+++ area.py
@@ -0,0 +1,25 @@
+def area_circle():
+	print("!...Area of circle...!")
+	r=float(input("enter radius:"))
+	area=3.14*r*r
+	print("area of circle",area)
+
+def area_sq():
+	print("!...Area of square...!")
+	s=float(input("enter side:"))
+	area=s*s
+	print("area of square:",area)
+
+def area_rect():
+	print("!...Area of rectangle...!")
+	l=float(input("enter length of rectangle:"))
+	b=float(input("enter breath of rectangle:"))
+	area=l*b
+	print("area of rectangle:",area)
+
+def area_tri():
+	print("!...Area of triangle...!")
+	h=float(input("enter height of rectangle:"))
+	b=float(input("enter breath of rectangle:"))
+	area=0.5*h*b
+	print("area of triangle:",area)
---
+++ module.py
@@ -0,0 +1,5 @@
+from area import*
+area_circle()
+area_sq()
+area_rect()
+area_tri()
